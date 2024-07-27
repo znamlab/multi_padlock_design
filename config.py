ac281d869241cbd6f7c8cf0560c152587035a1a0
AlexBecalick <alexander.becalic@crick.ac.uk>
2024-07-27 13:19:16 +0100

    [minor] update gitignore and config

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
 # maximum number of processes for parallel ClustalW
-max_MSA_processes = 6
+max_MSA_processes = 32
 
 # maximum number of processes for parallel Blast
-max_Blast_processes = 6
+max_Blast_processes = 32
 
 # mouse database
-fastadir_mouse = r'C:\Worky\DB\RefSeq'
+fastadir_mouse = (
+    r"/nemo/lab/znamenskiyp/home/users/becalia/code/multi_padlock_design/refseq"
+)
 fasta_filenum_mouse = 2
-fasta_pre_suffix_mouose = ('mouse.', '.rna.fna')
+fasta_pre_suffix_mouse = ("mouse.", ".rna.fna")
 
 # human database
-fastadir_human = r'C:\Worky\DB\RefSeq'
+fastadir_human = (
+    r"/nemo/lab/znamenskiyp/home/users/becalia/code/multi_padlock_design/refseq"
+)
 fasta_filenum_human = 7
-fasta_pre_suffix_human = ('human.', '.rna.fna')
+fasta_pre_suffix_human = ("human.", ".rna.fna")
